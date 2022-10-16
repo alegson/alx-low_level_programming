38de56feeacace68979740e2b75a6daea23d7289
Alege Emmanuel <alegzy11@gmail.com>
2022-10-16 22:17:31 +0000

    printing string with put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/*
+ * main - prints a string with the puts() function
+ *
+ * return - always 0
+ */
+int main()
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
