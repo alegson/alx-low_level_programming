c9af3b87b3343251aa3cdc4e4577c786b28b8b3b
Alege Emmanuel <alegzy11@gmail.com>
2022-10-17 22:07:57 +0000

    writing text on console without printf and puts

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <fcntl.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	char string[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+		write(1,string,59);
+			return (1);
+}
