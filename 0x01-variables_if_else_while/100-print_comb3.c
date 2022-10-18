a9c2710f2f85baa78fba43500f25646cfd492aba
Alege Emmanuel <alegzy11@gmail.com>
2022-10-18 00:56:48 +0000

    adding 100-print_comb3.c file

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,31 +6,29 @@
  */
 int main(void)
 {
-		int i, e;
-
-			i = 48;
-				e = 48;
-
-					while (e < 58)
+	int i, e;
+		i = 48;
+			e = 48;
+				while (e < 58)
+				{
+					i = 48;
+					while (i < 58)
 					{
-						i = 48;
-						while (i < 58)
+						if (e != i && e < i)
 						{
-							if (e != i && e < i)
-							{
-								putchar(e);
-									putchar(i);
-									if (i == 57 && e == 56)
-									{
-										break;
-									}
-									putchar(',');
-										putchar(' ');
-							}
-								i++;
+							putchar(e);
+								putchar(i);
+								if (i == 57 && e == 56)
+								{
+									break;
+								}
+								putchar(',');
+									putchar(' ');
 						}
-							e++;
+							i++;
 					}
-						putchar('\n');
-							return (0);
+						e++;
+				}
+					putchar('\n');
+						return (0);
 }
