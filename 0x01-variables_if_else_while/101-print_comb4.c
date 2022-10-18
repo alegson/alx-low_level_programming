4ab322e58c5cbb54fee9c00c8daff8b99b3d21b7
Alege Emmanuel <alegzy11@gmail.com>
2022-10-18 01:34:50 +0000

    adding the 101-print_comb4.c file

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,41 +7,31 @@
  */
 int main(void)
 {
-	int i, e, g, h, op1, op2;
-		i = e = g = h = 48;
-		while (h < 58)
+	int m, i, j;
+	for(i = '0'; i <= '9'; i++)
+	{
+		for(j = '0'; j <= '9'; j++)
 		{
-			g = 48;
-			while (g < 58)
+			for(m = '0'; m <= '9'; m++)
 			{
-				e = 48;
-				while (e < 58)
+				if (i < j && j < m)
 				{
-					i = 48;
-					while (i < 58)
-					{
-						op1 = (h * 10) + g;
-							op2 = (e * 10) + i;
-							if (op1 < op2)
+					putchar(i);
+						putchar(j);
+							putchar(m);
+							if (i == '7' && j == '8' && m == '9')
 							{
-								putchar(h);
-									putchar(g);
-										putchar(' ');
-											putchar(e);
-												putchar(i);
-												if (h == 57 && g == 56 && e == 57 && i == 57)
-													break;
-												putchar(',');
-													putchar(' ');
+								break;
 							}
-								i++;
-					}
-						e++;
+							else
+							{
+								putchar(',');
+									putchar(' ');
+							}
 				}
-					g++;
 			}
-				h++;
 		}
-			putchar('\n');
-				return (0);
+	}
+	putchar('\n');
+		return (0);
 }
