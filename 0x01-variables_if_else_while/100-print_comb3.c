376fea46b6240fb988c0999218ed0353bbf0316c
Alege Emmanuel <alegzy11@gmail.com>
2022-10-18 00:53:18 +0000

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -15,22 +15,22 @@ int main(void)
 					while (e < 58)
 					{
 						i = 48;
-							while (i < 58)
+						while (i < 58)
+						{
+							if (e != i && e < i)
 							{
-								if (e != i && e < i)
-								{
-									putchar(e);
-										putchar(i);
-											if (i == 57 && e == 56)
-											{
-												break;
-											}
-											putchar(',');
-												putchar(' ');
-								}
-									i++;
+								putchar(e);
+									putchar(i);
+									if (i == 57 && e == 56)
+									{
+										break;
+									}
+									putchar(',');
+										putchar(' ');
 							}
-								e++;
+								i++;
+						}
+							e++;
 					}
 						putchar('\n');
 							return (0);
